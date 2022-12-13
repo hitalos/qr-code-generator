78320ca864e237773849cf360d631f3617c0e64b
Hítalo Silva <hitalos@gmail.com>
2022-12-13 15:56:15 -0300

    Improvements on handle errors

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -15,31 +15,44 @@ import (
 func Index(engine *html.Engine) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := engine.Render(w, "index", nil, "layouts/main"); err != nil {
-			log.Println(err)
+			errHandler(err, w)
 		}
 	}
 }
 
-func PNGimage(w http.ResponseWriter, r *http.Request) {
-	str := chi.URLParam(r, "str")
-	qr, _ := qr.Encode(str, qr.M, qr.Auto)
-	size := qr.Bounds().Max.X * 20
-	qr, _ = barcode.Scale(qr, size, size)
+func createBarcode(r *http.Request, scale int) (barcode.Barcode, error) {
+	qrcode, err := qr.Encode(chi.URLParam(r, "str"), qr.M, qr.Auto)
+	if err != nil {
+		return qrcode, err
+	}
 
-	if err := png.Encode(w, qr); err != nil {
-		log.Println(err)
+	if scale > 1 {
+		size := qrcode.Bounds().Max.X * scale
+		qrcode, err = barcode.Scale(qrcode, size, size)
+	}
+
+	return qrcode, err
+}
+
+func PNGimage(w http.ResponseWriter, r *http.Request) {
+	qrcode, err := createBarcode(r, 20)
+	if err != nil {
+		errHandler(err, w)
+	}
+
+	if err = png.Encode(w, qrcode); err != nil {
+		errHandler(err, w)
 	}
 }
 
 func SVGimage(w http.ResponseWriter, r *http.Request) {
-	str := chi.URLParam(r, "str")
-	qr, err := qr.Encode(str, qr.M, qr.Auto)
+	qrcode, err := createBarcode(r, 1)
 	if err != nil {
-		log.Println(err)
+		errHandler(err, w)
 	}
 
 	qs := qrSVG{
-		qr:        qr,
+		qr:        qrcode,
 		blockSize: 10,
 	}
 
@@ -47,7 +60,13 @@ func SVGimage(w http.ResponseWriter, r *http.Request) {
 	s := svg.New(w)
 	defer s.End()
 
-	if err = qs.WriteQrSVG(s); err != nil {
-		log.Println(err)
+	if err := qs.WriteQrSVG(s); err != nil {
+		errHandler(err, w)
 	}
+}
+
+func errHandler(err error, w http.ResponseWriter) {
+	log.Println(err)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("error!"))
 }
