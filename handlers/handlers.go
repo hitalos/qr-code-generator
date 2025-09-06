093801fb50f5f02e004898b2be16e82b112199a9
Hítalo Silva <hitalos@gmail.com>
2025-09-06 04:10:45 -0300

    Refactor PNG image generation to include a bordered background

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"image"
+	"image/color"
+	"image/draw"
 	"image/png"
 	"log"
 	"net/http"
@@ -27,7 +30,7 @@ func createBarcode(r *http.Request, scale int) (barcode.Barcode, error) {
 		return nil, err
 	}
 
-	qrcode, err := qr.Encode(str, qr.M, qr.Auto)
+	qrcode, err := qr.Encode(str, qr.L, qr.Auto)
 	if err != nil {
 		return qrcode, err
 	}
@@ -46,7 +49,13 @@ func PNGimage(w http.ResponseWriter, r *http.Request) {
 		errHandler(err, w)
 	}
 
-	if err = png.Encode(w, qrcode); err != nil {
+	border := 20
+	bColor := color.RGBA{255, 255, 255, 255}
+	borderedImage := image.NewRGBA(image.Rect(0, 0, qrcode.Bounds().Dx()+2*border, qrcode.Bounds().Dy()+2*border))
+	draw.Draw(borderedImage, borderedImage.Bounds(), &image.Uniform{C: bColor}, image.Point{}, draw.Src)
+	draw.Draw(borderedImage, image.Rect(border, border, qrcode.Bounds().Dx()+border, qrcode.Bounds().Dy()+border), qrcode, image.Point{}, draw.Src)
+
+	if err = png.Encode(w, borderedImage); err != nil {
 		errHandler(err, w)
 	}
 }
